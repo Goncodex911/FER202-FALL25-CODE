6eb6dc6c4c8d6a2349a0f326e4cad64da83cf031
Goncodex911 <nguyenhieuthuan0301qb@gmail.com>
2025-10-15 09:42:45 +0700

    my movie

---
+++ my-movie-app/src/pages/FooterPage.jsx
@@ -0,0 +1,9 @@
+import MyFooter from "../components/Footer/MyFooter"; 
+
+export default function FooterPage() { 
+  return (
+    <div> 
+      <MyFooter author="Thuận Nguyễn" email="nguyenhieuthuan0301qb@gmail.com" linkGithub="Movie Management Project" /> 
+    </div> 
+  );
+}
